42b3952812d70025f3561946585f2ce32896b6ae
William Block <wtblock@hotmail.com>
2020-08-04 12:07:14 -0500

    Initial design.

--- FindMissingDateTaken/FindMissingDateTaken.cpp
+++ FindMissingDateTaken/FindMissingDateTaken.cpp
@@ -1,5 +1,6 @@
-// FindMissingDateTaken.cpp : Defines the entry point for the console application.
-//
+/////////////////////////////////////////////////////////////////////////////
+// Copyright © by W. T. Block, all rights reserved
+/////////////////////////////////////////////////////////////////////////////
 
 #include "stdafx.h"
 #include "FindMissingDateTaken.h"
@@ -9,38 +10,213 @@
 #endif
 
 
+/////////////////////////////////////////////////////////////////////////////
 // The one and only application object
-
 CWinApp theApp;
 
-using namespace std;
-
-int main()
+/////////////////////////////////////////////////////////////////////////////
+bool GetDateFound( Gdiplus::Image* pImage, PROPID id )
 {
-    int nRetCode = 0;
+	bool value = false;
 
-    HMODULE hModule = ::GetModuleHandle(nullptr);
+	// get the size of the date property
+	const UINT uiSize = pImage->GetPropertyItemSize( id );
 
-    if (hModule != nullptr)
-    {
-        // initialize MFC and print and error on failure
-        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
-        {
-            // TODO: change error code to suit your needs
-            wprintf(L"Fatal Error: MFC initialization failed\n");
-            nRetCode = 1;
-        }
-        else
-        {
-            // TODO: code your application's behavior here.
-        }
-    }
-    else
-    {
-        // TODO: change error code to suit your needs
-        wprintf(L"Fatal Error: GetModuleHandle failed\n");
-        nRetCode = 1;
-    }
+	// if the property exists, it will have a non-zero size 
+	if ( uiSize > 0 )
+	{
+		// using a smart pointer which will release itself
+		// when it goes out of context
+		unique_ptr<Gdiplus::PropertyItem> pItem =
+			unique_ptr<Gdiplus::PropertyItem>
+			(
+				(PropertyItem*)malloc( uiSize )
+			);
 
-    return nRetCode;
-}
+		// Get the property item.
+		pImage->GetPropertyItem
+		(
+			PropertyTagExifDTOrig, uiSize, pItem.get()
+		);
+
+		// the property should be ASCII
+		if ( pItem->type == PropertyTagTypeASCII )
+		{
+			value = true;
+		}
+	}
+
+	return value;
+} // GetDateFound
+
+/////////////////////////////////////////////////////////////////////////////
+// crawl through the directory tree
+void RecursePath( LPCTSTR path )
+{
+	USES_CONVERSION;
+
+	// valid file extensions
+	const CString csValidExt = _T( ".jpg;.jpeg;.png;.gif;.bmp;.tif;.tiff" );
+
+	// the new folder under the image folder to contain the corrected images
+	CString csPathname( path );
+	csPathname.TrimRight( _T( "\\" ) );
+
+	// build a string with wildcards
+	CString strWildcard;
+	strWildcard.Format( _T( "%s\\*.*" ), path );
+
+	// start trolling for files we are interested in
+	CFileFind finder;
+	BOOL bWorking = finder.FindFile( strWildcard );
+	while ( bWorking )
+	{
+		bWorking = finder.FindNextFile();
+
+		// skip "." and ".." folder names
+		if ( finder.IsDots() )
+		{
+			continue;
+		}
+
+		// if it's a directory, recursively search it
+		if ( finder.IsDirectory() )
+		{
+			const CString str = finder.GetFilePath();
+
+			// recurse into the new directory
+			RecursePath( str );
+
+		} else // read the properties if it is a valid extension
+		{
+			const CString csPath = finder.GetFilePath();
+			const CString csExt = GetExtension( csPath ).MakeLower();
+
+			if ( -1 != csValidExt.Find( csExt ) )
+			{
+				// smart pointer to the image representing this element
+				unique_ptr<Gdiplus::Image> pImage =
+					unique_ptr<Gdiplus::Image>
+					(
+						Gdiplus::Image::FromFile( T2CW( csPath ) )
+					);
+
+				// being pessimistic
+				bool bOkay = false;
+
+				// test for original date
+				if ( GetDateFound( pImage.get(), PropertyTagExifDTOrig ))
+				{
+					bOkay = true;
+				}
+
+				// test for digitized date
+				if ( GetDateFound( pImage.get(), PropertyTagExifDTDigitized ))
+				{
+					bOkay = true;
+				}
+
+				// if not okay, the date information is missing so record
+				// the pathname to the output
+				if ( bOkay == false )
+				{
+					CStdioFile fout( stdout );
+
+					fout.WriteString( csPath + _T( "\n" ) );
+				}
+			}
+		}
+	}
+
+	finder.Close();
+
+} // RecursePath
+
+/////////////////////////////////////////////////////////////////////////////
+// a console application that can crawl through the file
+// system and troll for image properties
+int _tmain( int argc, TCHAR* argv[], TCHAR* envp[] )
+{
+	HMODULE hModule = ::GetModuleHandle( NULL );
+	if ( hModule == NULL )
+	{
+		_tprintf( _T( "Fatal Error: GetModuleHandle failed\n" ) );
+		return 1;
+	}
+
+	// initialize MFC and error on failure
+	if ( !AfxWinInit( hModule, NULL, ::GetCommandLine(), 0 ) )
+	{
+		_tprintf( _T( "Fatal Error: MFC initialization failed\n " ) );
+		return 2;
+	}
+
+	CStdioFile fOut( stdout );
+	if ( argc != 2 )
+	{
+		fOut.WriteString( _T( ".\n" ) );
+		fOut.WriteString
+		(
+			_T( "FindMissingDateTaken, Copyright (c) 2020, " )
+			_T( "by W. T. Block.\n" )
+		);
+		fOut.WriteString( _T( ".\n" ) );
+		fOut.WriteString( _T( "Usage:\n" ) );
+		fOut.WriteString( _T( ".\n" ) );
+		fOut.WriteString( _T( ".  FindMissingDateTaken pathname\n" ) );
+		fOut.WriteString( _T( ".\n" ) );
+		fOut.WriteString( _T( "Where:\n" ) );
+		fOut.WriteString( _T( ".\n" ) );
+		fOut.WriteString
+		(
+			_T( ".  pathname is the root of the tree to be scanned\n" )
+		);
+		fOut.WriteString( _T( ".\n" ) );
+		return 3;
+	}
+
+	// display the executable path
+	CString csMessage;
+	csMessage.Format( _T( "Executable pathname: %s\n" ), argv[ 0 ] );
+	fOut.WriteString( _T( ".\n" ) );
+	fOut.WriteString( csMessage );
+	fOut.WriteString( _T( ".\n" ) );
+
+	// retrieve the pathname and validate the pathname exists
+	CString csPath = argv[ 1 ];
+	if ( !::PathFileExists( csPath ) )
+	{
+		csMessage.Format( _T( "Invalid pathname: %s\n" ), csPath );
+		fOut.WriteString( _T( ".\n" ) );
+		fOut.WriteString( csMessage );
+		fOut.WriteString( _T( ".\n" ) );
+		return 4;
+
+	} else
+	{
+		csMessage.Format( _T( "Given pathname: %s\n" ), csPath );
+		fOut.WriteString( _T( ".\n" ) );
+		fOut.WriteString( csMessage );
+		fOut.WriteString( _T( ".\n" ) );
+	}
+
+	// start up COM
+	AfxOleInit();
+	::CoInitialize( NULL );
+
+	// reference to GDI+
+	InitGdiplus();
+
+	// crawl through directory tree defined by the command line
+	// parameter trolling for image files
+	RecursePath( csPath );
+
+	// clean up references to GDI+
+	TerminateGdiplus();
+
+	// all is good
+	return 0;
+
+} // _tmain
+
+/////////////////////////////////////////////////////////////////////////////
--- FindMissingDateTaken/FindMissingDateTaken.h
+++ FindMissingDateTaken/FindMissingDateTaken.h
@@ -1,3 +1,125 @@
+/////////////////////////////////////////////////////////////////////////////
+// Copyright © by W. T. Block, all rights reserved
+/////////////////////////////////////////////////////////////////////////////
 #pragma once
 
 #include "resource.h"
+#include <vector>
+#include <map>
+#include <memory>
+#include <gdiplus.h>
+#pragma comment(lib, "gdiplus.lib")
+
+using namespace Gdiplus;
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////
+// used for gdiplus libraray
+ULONG_PTR m_gdiplusToken;
+
+/////////////////////////////////////////////////////////////////////////////
+// parse the filename from a pathname
+static inline CString GetFileName( LPCTSTR pcszPath )
+{
+	CString csPath( pcszPath );
+	TCHAR* pBuf = csPath.GetBuffer( csPath.GetLength() + 1 );
+	TCHAR szDrive[ _MAX_DRIVE ];
+	TCHAR szDir[ _MAX_DIR ];
+	TCHAR szFile[ _MAX_FNAME ];
+	TCHAR szExt[ _MAX_EXT ];
+
+	_tsplitpath( pBuf, szDrive, szDir, szFile, szExt );
+	csPath.ReleaseBuffer();
+	return szFile;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// parse the extension from a pathname
+static inline CString GetExtension( LPCTSTR pcszPath )
+{
+	CString csPath( pcszPath );
+	TCHAR* pBuf = csPath.GetBuffer( csPath.GetLength() + 1 );
+	TCHAR szDrive[ _MAX_DRIVE ];
+	TCHAR szDir[ _MAX_DIR ];
+	TCHAR szFile[ _MAX_FNAME ];
+	TCHAR szExt[ _MAX_EXT ];
+
+	_tsplitpath( pBuf, szDrive, szDir, szFile, szExt );
+	csPath.ReleaseBuffer();
+	return szExt;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// parse the directory from a pathname
+static inline CString GetDirectory( LPCTSTR pcszPath )
+{
+	CString csPath( pcszPath );
+	TCHAR* pBuf = csPath.GetBuffer( csPath.GetLength() + 1 );
+	TCHAR szDrive[ _MAX_DRIVE ];
+	TCHAR szDir[ _MAX_DIR ];
+	TCHAR szFile[ _MAX_FNAME ];
+	TCHAR szExt[ _MAX_EXT ];
+
+	_tsplitpath( pBuf, szDrive, szDir, szFile, szExt );
+	csPath.ReleaseBuffer();
+	return szDir;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// parse the drive from a pathname
+static inline CString GetDrive( LPCTSTR pcszPath )
+{
+	CString csPath( pcszPath );
+	TCHAR* pBuf = csPath.GetBuffer( csPath.GetLength() + 1 );
+	TCHAR szDrive[ _MAX_DRIVE ];
+	TCHAR szDir[ _MAX_DIR ];
+	TCHAR szFile[ _MAX_FNAME ];
+	TCHAR szExt[ _MAX_EXT ];
+
+	_tsplitpath( pBuf, szDrive, szDir, szFile, szExt );
+	csPath.ReleaseBuffer();
+	return szDrive;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// parse folder from a pathname (drive and directory)
+static inline CString GetFolder( LPCTSTR pcszPath )
+{
+	CString csDrive = GetDrive( pcszPath );
+	CString csDir = GetDirectory( pcszPath );
+	return csDrive + csDir;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// parse data name from a pathname (filename and extension)
+static inline CString GetDataName( LPCTSTR pcszPath )
+{
+	CString csFile = GetFileName( pcszPath );
+	CString csExt = GetExtension( pcszPath );
+	return csFile + csExt;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// initialize GDI+
+bool InitGdiplus()
+{
+	GdiplusStartupInput gdiplusStartupInput;
+	Status status = GdiplusStartup
+	(
+		&m_gdiplusToken,
+		&gdiplusStartupInput,
+		NULL
+	);
+	return ( Ok == status );
+} // InitGdiplus
+
+/////////////////////////////////////////////////////////////////////////////
+// remove reference to GDI+
+void TerminateGdiplus()
+{
+	GdiplusShutdown( m_gdiplusToken );
+	m_gdiplusToken = NULL;
+
+}// TerminateGdiplus
+
+/////////////////////////////////////////////////////////////////////////////
